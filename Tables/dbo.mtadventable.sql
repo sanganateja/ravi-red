8a9cd795f2a311210ea7e1205ead1d1bd7910a3e
Ravi <sangana575@gmail.com>
2020-08-05 16:36:56 +0530

    Added-myfrist-table

---
+++ Tables/dbo.mtadventable.sql
@@ -0,0 +1,7 @@
+CREATE TABLE [dbo].[mtadventable]
+(
+[sno] [nchar] (10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[emp] [nchar] (10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[id] [nchar] (10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
+) ON [PRIMARY]
+GO
